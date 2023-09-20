1d668e52affafc9ea498952ed4efeb43fca8aa4f
Joaquin Diaz <diazjoaquin1997@gmail.com>
2023-09-20 15:00:32 -0300

    Agrego controllador Clients

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using Curso_TOM.Models.Clients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Curso_TOM.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ClientViewModel clientViewModel = new ClientViewModel() {
+                ClientBirth = new DateTime(1997, 10, 14),
+                ClientFullName = "Joaquin Diaz",
+                ClientId = 5402002
+            };
+
+            return View(clientViewModel);
+        }
+    }
+}
---
+++ Models/Clients/ClientViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Curso_TOM.Models.Clients
+{
+    public class ClientViewModel
+    {
+        public int ClientId { get; set; }
+        public string ClientFullName { get; set; }
+        public DateTime ClientBirth { get; set; }
+    }
+}
