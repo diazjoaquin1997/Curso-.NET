b33bbba0e26d9c2d203562b2ad412e30a962ba79
Joaquin Diaz <diazjoaquin1997@gmail.com>
2023-10-09 11:46:40 -0300

    Agrego WCF Service Library

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -5,12 +5,18 @@ namespace Curso_TOM.Controllers
 {
     public class ClientsController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            //TOMService.Service1Client service = new TOMService.Service1Client();
+            //var x = await service.GetDataAsync(1);
+
+            ServiceReference1.ServiceClient serviceReference = new ServiceReference1.ServiceClient();
+            string nombre = serviceReference.GetNombreAsync(40420273).Result;
+
             ClientViewModel clientViewModel = new ClientViewModel() {
                 ClientBirth = new DateTime(1997, 10, 14),
-                ClientFullName = "Joaquin Diaz",
-                ClientId = 5402002
+                ClientFullName = nombre,
+                ClientId = 213
             };
 
             return View(clientViewModel);
